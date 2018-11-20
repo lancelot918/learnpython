6e56f138403ac64e2b88cc3d6e314015fc0452e9
lancelot <lancelot918@gmail.com>
2018-11-20 16:53:07 +0800

    modified decorator return

--- decorator/decor_arg.py
+++ decorator/decor_arg.py
@@ -4,9 +4,25 @@ def do_twice(func):
         func(*args, **kwargs)
     return wrapper_do_twice
 
+def do_twice_ex(func):
+    def wrapper_do_twice(*args, **kwargs):
+        func(*args, **kwargs)
+        return func(*args, **kwargs)
+    return wrapper_do_twice
+
 @do_twice
 def say_greeting(name):
     print(f"Great greeting, {name}")
+    return f'{name}'
+
+@do_twice_ex
+def say_greeting_ex(name):
+    print(f"Great greeting, {name}")
+    return f'{name}'
 
 if __name__ == '__main__'    :
-    say_greeting('Peter')
+    hi_peter = say_greeting('Peter')
+    print(hi_peter)
+
+    hi_coffee = say_greeting_ex('Coffee')
+    print(hi_coffee)
